aed7b12099807910f9864074b0c28e4038f07a07
Crowdbotics <team@crowdbotics.com>
2024-01-23 04:25:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-dream-45479",
+  url: "https://jolly-dream-45479.botics.co",
+  api: "https://jolly-dream-45479.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
